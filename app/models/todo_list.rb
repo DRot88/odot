59b81c1a1428b9436fd135f908f3656a56c69d28
Daniel Rotenberg <Daniel.Rotenberg88@gmail.com>
2015-09-04 18:27:58 -0400

    Add todo item model and associated specs

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,5 +1,9 @@
 class TodoList < ActiveRecord::Base
+  has_many :todo_items
+
   validates :title, presence: true, length: { minimum: 3}
   validates :description, presence: true, length: { minimum: 5 }
+
+
 
 end
